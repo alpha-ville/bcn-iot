efb03257046e851c9ebf2fd6130b9aefd44361e7
Silvio Paganini <s2paganini@gmail.com>
2015-02-06 23:26:04 +0000

    modernizr

---
+++ gulp/tasks/modernizr.js
@@ -0,0 +1,10 @@
+var gulp      = require('gulp');
+var modernizr = require('gulp-modernizr');
+var pkg       = require('../../package.json');
+
+gulp.task('modernizr', function() {
+  gulp.src(pkg.folders.dest+'/static/js/*.js')
+    .pipe(modernizr())
+    .pipe(uglify())
+    .pipe(gulp.dest(pkg.folders.dest+'/static/js/vendor/'))
+});
