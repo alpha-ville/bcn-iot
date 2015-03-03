2da85fa7e1e2ee29e1c15a703f524e4871c54744
William <william.mapan>
2015-03-03 22:19:11 +0100

    no idea

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.header("Pragma", "no-cache");
+  res.header("Expires", 0);
+  
   res.render('index', { title: 'BCN IoT' });
 });
 
